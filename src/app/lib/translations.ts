a582aa4f960e4201566928ee213b4a71c1752e61
eduardoamdev <eduardoamdev@gmail.com>
2025-06-15 19:23:05 +0200

    Basic home page translation implemented

---
+++ src/app/lib/translations.ts
@@ -0,0 +1,10 @@
+export const translations = {
+  en: {
+    title: "Data Project",
+  },
+  ka: {
+    title: "მონაცემთა პროექტი",
+  },
+};
+
+export type SupportedLocale = keyof typeof translations;
