b78d4f53592e604840d13e9f24f7e55766d5ab27
eduardoamdev <eduardoamdev@gmail.com>
2025-06-15 18:36:13 +0200

    i18n middleware implemented

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,32 @@
-export function middleware() {
-  console.log("Middleware running");
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const supportedLocales: Array<string> = ["en", "ka"];
+
+const defaultLocale: string = "en";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const pathnameParts: string[] = pathname.split("/");
+
+  const locale: string = pathnameParts[1];
+
+  if (!supportedLocales.includes(locale)) {
+    console.log("Locale not supported, redirecting to default");
+
+    const newUrl = request.nextUrl.clone();
+
+    newUrl.pathname = `/${defaultLocale}${pathname}`;
+
+    return NextResponse.redirect(newUrl);
+  }
+
+  console.log(`Middleware running with locale: ${locale}`);
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/",
+  matcher: "/:path*",
 };
