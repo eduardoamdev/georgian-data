1a43c37beee1810801aa417606d7af8d475877a0
eduardoamdev <eduardoamdev@gmail.com>
2025-06-15 19:24:01 +0200

    Translated texts added to the home page and Link to swith implemented

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>Data Project</h1>;
+import Link from "next/link";
+import { translations, SupportedLocale } from "../../lib/translations";
+
+export default function Home({
+  params,
+}: {
+  params: { locale: SupportedLocale };
+}) {
+  // Link to home page in the locale which is not the current one
+  const otherLocale = params.locale === "en" ? "ka" : "en";
+  const otherLocaleLink = `/${otherLocale}/home`;
+
+  const t = translations[params.locale] || translations.en;
+
+  return (
+    <div>
+      <Link href={otherLocaleLink}>{otherLocale}</Link>
+      <h1>{t.title}</h1>
+    </div>
+  );
 }
